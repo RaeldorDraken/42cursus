c07ffdb858e18e779b5b6de492ff01f2123b87fb
Eduard Ros giralt <eros-gir@car3s1.42barcelona.com>
2022-01-25 14:48:29 +0100

    redo calloc for 0 size case

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/16 18:59:59 by eros-gir          #+#    #+#             */
-/*   Updated: 2022/01/17 19:15:27 by eros-gir         ###   ########.fr       */
+/*   Updated: 2022/01/25 14:43:38 by eros-gir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,13 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	size_t		len;
-	size_t		fsize;
-	void		*result;
-	char		*setzero;
+	size_t	fsize;
+	void	*result;
 
-	fsize = (long int)(nmemb * size);
-	if (fsize == 0)
-	{
-		return (0);
-	}
+	fsize = nmemb * size;
 	result = malloc(nmemb * size);
 	if (!result)
 		return (0);
-	setzero = (char *)result;
-	if (fsize != 0)
-	{
-		len = -1;
-		while (++len < fsize)
-			setzero[len] = 0;
-	}
-	result = (void *)setzero;
+	ft_bzero(result, fsize);
 	return (result);
 }
