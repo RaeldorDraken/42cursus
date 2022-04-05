26c084e5496347248d136a93f1118018840d7718
Eduard Ros giralt <eros-gir@car3s1.42barcelona.com>
2022-04-05 13:10:46 +0200

    this is an update to show stuff on screen

--- so_long/main.c
+++ so_long/main.c
@@ -6,31 +6,73 @@
 /*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 11:27:44 by eros-gir          #+#    #+#             */
-/*   Updated: 2022/03/31 12:03:18 by eros-gir         ###   ########.fr       */
+/*   Updated: 2022/04/05 13:09:00 by eros-gir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"libft/libft.h"
-#include"minilibx/mlx.h"
-#include<stdio.h>
+#include"utilslib.h"
+
+void	draw_square(t_data *data, int x, int y, int w, int h, int color)
+{
+	int startx;
+
+	startx = x;
+	w = w + x;
+	h = h + y;
+	while (y < h)
+	{
+		while (x < w)
+		{
+			put_pixel(data, x, y, color);
+			x ++;
+		}
+		x = startx;
+		y ++;
+	}		
+}
+
+void	draw_gradient(t_data *data, int w, int h)
+{
+	int	color;
+	int	x;
+	int	y;
+	int	red = 0;
+	int	green = 0;
+	int	blue = 0;
+
+	color = rgbencode(red, green, blue);
+	x = 0;
+	y = 0;
+	while(x < w)
+	{
+		while (y < h)
+		{
+			put_pixel(data, x, y, color);
+			y ++;
+		}
+		red ++;
+		green ++;
+		blue ++;
+		y = 0;
+		color = rgbencode(red, green, blue);
+		x ++;
+	}
+}
+
+//void	draw_circle(t_data *data, int x, int y, int rad, int color)
 
 int	main(void)
 {
 	void	*mlxptr;
-	int		count;
+	void	*mlx_win;
+	t_data	img;
 
-	count = 1000;
 	mlxptr = mlx_init();
-	if (!mlxptr)
-	{
-		printf("ERROR ON INIT");
-		return (0);
-	}
-	if (!mlx_new_window(mlxptr, 1280, 720, "TEST WINDOW"))
-	{
-		printf("ERROR ON CREATE WINDOW");
-		return (0);
-	}
-	while (count > 0)
-		count --;
+	mlx_win = mlx_new_window(mlxptr, 1280, 720, "TEST WINDOW");
+	img.img = mlx_new_image(mlxptr, 1280, 720);
+	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.linelen, &img.endian);
+	draw_gradient(&img, 1280, 720);
+	draw_square(&img, 640, 360, 64, 64, 0x00FF0000);
+	mlx_put_image_to_window(mlxptr, mlx_win, img.img, 0, 0);
+	mlx_loop(mlxptr);
 }
--- so_long/utilslib.h
+++ so_long/utilslib.h
@@ -1,26 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utilities.c                                        :+:      :+:    :+:   */
+/*   utilslib.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/05 11:13:05 by eros-gir          #+#    #+#             */
-/*   Updated: 2022/04/05 12:27:11 by eros-gir         ###   ########.fr       */
+/*   Created: 2022/04/05 11:14:05 by eros-gir          #+#    #+#             */
+/*   Updated: 2022/04/05 12:21:57 by eros-gir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"utilslib.h"
+#ifndef UTILSLIB_H
+# define UTILSLIB_H
 
-void	put_pixel(t_data *data, int x, int y, int color)
-{
-	char	*dst;
+# include"libft/libft.h"
+# include"minilibx/mlx.h"
+# include<stdio.h>
 
-	dst = data->addr + (y * data->linelen + x * (data->bpp / 8));
-	*(unsigned int *)dst = color;
-}
+typedef struct s_data {
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		linelen;
+	int		endian;
+}	t_data;
 
-int	rgbencode(uint8_t red, uint8_t green, uint8_t blue)
-{
-	return (red << 16 | green << 8 | blue);
-}
+void	put_pixel(t_data *data, int x, int y, int color);
+int		rgbencode(uint8_t red, uint8_t green, uint8_t blue);
+
+#endif
