5bdaf37a5072dfacc436a4fd75e0943223571664
Eduard Ros giralt <eros-gir@car3s1.42barcelona.com>
2022-05-21 12:39:16 +0200

    updating libft with new functions

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 12:19:42 by eros-gir          #+#    #+#             */
-/*   Updated: 2022/01/20 11:42:27 by eros-gir         ###   ########.fr       */
+/*   Updated: 2022/04/12 10:25:32 by eros-gir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ char	*ft_strtrim(const char *s1, const char *set)
 	if (!s1 || !set)
 		return (0);
 	slen = 0;
-	while (s1[slen] && ft_strchr(set, s1[slen]))
+	while (s1[slen] && ft_strchr((char *)set, s1[slen]))
 		slen++;
 	flen = ft_strlen(s1);
-	while (flen > slen && ft_strchr(set, s1[flen]))
+	while (flen > slen && ft_strchr((char *)set, s1[flen]))
 		flen--;
 	if (slen > flen)
 		return (ft_strdup(""));
