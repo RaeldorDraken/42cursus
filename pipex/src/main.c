ac583c06d8b3686cc618e0b580f1f969422a3bd1
Eduard Ros giralt <eros-gir@car3s1.42barcelona.com>
2022-06-02 12:41:38 +0200

    Pipex mostly works, tons of individaul errors but at least the pipe is working

--- pipex/src/main.c
+++ pipex/src/main.c
@@ -6,81 +6,94 @@
 /*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 09:50:56 by eros-gir          #+#    #+#             */
-/*   Updated: 2022/05/31 13:03:43 by eros-gir         ###   ########.fr       */
+/*   Updated: 2022/06/02 12:36:19 by eros-gir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"pipexlib.h"
 
-void	child_process(t_pipex *pobj, char **envp)
+void	close_fds(t_pipex *pobj)
+{
+	close(pobj->end[0]);
+	close(pobj->infile);
+	close(pobj->end[1]);
+	close(pobj->outfile);
+}
+
+void	child1_process(t_pipex *pobj, char **envp)
 {
 	int		i;
 	char	*cmd;
 
 	i = -1;
-	if (dup2(pobj->infile, STDIN_FILENO) < 0)
-	{
-		close(pobj->infile);
-		exit(EXIT_FAILURE);
-	}
+	dup2(pobj->infile, STDIN_FILENO);
 	dup2(pobj->end[1], STDOUT_FILENO);
 	while (pobj->paths[++i])
 	{
 		cmd = ft_strjoin(pobj->paths[i], pobj->command1[0]);
-		execve(cmd, pobj->command1, envp);
-		perror("exeve ERROR");
+		if (!access(cmd, F_OK))
+		{
+			close(pobj->end[0]);
+			execve(cmd, pobj->command1, envp);
+			perror("child2_exeve ERROR");
+		}
 		free(cmd);
 	}
-	close(pobj->end[0]);
-	close(pobj->infile);
+	close_fds(pobj);
 }
 
-void	parent_process(t_pipex *pobj)
+void	child2_process(t_pipex *pobj, char **envp)
 {
-	int	status;
+	int		i;
+	char	*cmd;
 
-	waitpid(-1, &status, 0);
-	if (dup2(pobj->outfile, STDOUT_FILENO) < 0)
-	{
-		close(pobj->outfile);
-		exit(EXIT_FAILURE);
-	}
+	i = -1;
+	dup2(pobj->outfile, STDOUT_FILENO);
 	dup2(pobj->end[0], STDIN_FILENO);
-	close(pobj->end[1]);
-	close(pobj->outfile);
+	while (pobj->paths[++i])
+	{
+		cmd = ft_strjoin(pobj->paths[i], pobj->command2[0]);
+		if (!access(cmd, F_OK))
+		{
+			close(pobj->end[1]);
+			execve(cmd, pobj->command2, envp);
+			perror("child2_exeve ERROR");
+		}
+		free(cmd);
+	}
+	close_fds(pobj);
 }
 
 void	pipex(t_pipex *pobj, char **envp)
 {
-	pid_t	parent;
+	int		status;
+	pid_t	child1;
+	pid_t	child2;
 
 	pipe(pobj->end);
-	parent = fork();
-	if (parent < 0)
+	child1 = fork();
+	if (child1 < 0)
 		return (perror("Fork: "));
-	if (!parent)
-		child_process(pobj, envp);
-	else
-		parent_process(pobj);
+	if (child1 == 0)
+		child1_process(pobj, envp);
+	child2 = fork();
+	if (child2 < 0)
+		return (perror("Fork: "));
+	if (child2 == 0)
+		child2_process(pobj, envp);
+	close(pobj->end[0]);
+	close(pobj->end[1]);
+	waitpid(child1, &status, 0);
+	waitpid(child2, &status, 0);
 }
 
 int	main(int ac, char **av, char **envp)
 {
 	t_pipex	pobj;
-	int		i;
 
-	i = 0;
 	if (ac != 5)
 		argerror(ac);
 	parse(&pobj, envp, av);
-	while (pobj.paths[i])
-		ft_putendl_fd(ft_strjoin(pobj.paths[i++], pobj.command1[0]), 1);
-	i = 0;
-	while (pobj.command1[i])
-		ft_putendl_fd(pobj.command1[i++], 1);
-	i = 0;
-	while (pobj.command2[i])
-		ft_putendl_fd(pobj.command2[i++], 1);
 	if (pobj.infile < 0 || pobj.outfile < 0)
 		exit(EXIT_FAILURE);
 	pipex(&pobj, envp);
