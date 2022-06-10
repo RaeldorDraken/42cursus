747cf7e72091ff35d6cfdb98790a816abe1150ac
Eduard Ros giralt <eros-gir@car3s1.42barcelona.com>
2022-06-10 11:06:21 +0200

    specila split work

--- test_fts/ft_pstrnum.c
+++ test_fts/ft_pstrnum.c
@@ -1,30 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pstrnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/10 10:08:13 by eros-gir          #+#    #+#             */
+/*   Updated: 2022/06/10 11:05:18 by eros-gir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include<stdio.h>
 
-size_t  ft_pstrnum(const char *s, char c)
+void	set_vars(size_t *len, size_t *numb, int *quotes)
+{
+	*len = 0;
+	*numb = 0;
+	*quotes = 0;
+}
+
+size_t	add_numb(int quotes)
+{
+	if (quotes <= 0)
+		return (1);
+	return (0);
+}
+
+size_t	ft_pstrnum(const char *s, char c)
 {
 	size_t	len;
 	size_t	numb;
 	int		quotes;
 
-	len = 0;
-	numb = 0;
-	quotes = 0;
+	set_vars(&len, &numb, &quotes);
 	while (s[len])
 	{
-		if (s[len] == '\'' || s[len] == '\"')
-		{
-			if (quotes <= 0)
-				numb ++;
-			quotes ++;
-		}
 		while (s[len] == c || s[len] == '\'' || s[len] == '\"')
 		{	
 			if (s[len] == '\'' || s[len] == '\"')
-			{
-				if (quotes <= 0)
-					numb ++;
+				numb += add_numb(quotes);
+			if (s[len] == '\'' || s[len] == '\"')
 				quotes ++;
-			}
 			len++;
 		}
 		if (s[len] && (s[len] != c && quotes <= 0))
@@ -41,14 +57,17 @@ size_t  ft_pstrnum(const char *s, char c)
 
 int	main(void)
 {
-	char	*str = "this string has only words";
+	char	*str;
 
+	str = "this string has only words";
 	printf("%s : %zu\n", str, ft_pstrnum(str, ' '));
 	str = "This string has some more words";
 	printf("%s : %zu\n", str, ft_pstrnum(str, ' '));
-	str = "This string has \'quotes in it\' fuck";
+	str = "This string has \'quotes in it\'";
 	printf("%s : %zu\n", str, ft_pstrnum(str, ' '));
-	str = "This string has double \'\"quotes in it\"\'";
+	str = "This string has double \'\"quotes in\"\' it";
+	printf("%s : %zu\n", str, ft_pstrnum(str, ' '));
+	str = "\'\"This string has\"\' two double \'qote quoet quote\'";
 	printf("%s : %zu\n", str, ft_pstrnum(str, ' '));
 	return (0);
 }
