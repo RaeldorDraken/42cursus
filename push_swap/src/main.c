e888651ef2ae388c5a21a4a20423bf79b338435e
Raeldor <maxshadow@hotmail.com>
2022-08-11 21:26:55 +0200

    changed the linked lists to arrays and started doing the swap functions, there is a need to check if the int arrays are handling 0 as a valid number or end of array

--- push_swap/src/main.c
+++ push_swap/src/main.c
@@ -24,12 +24,88 @@ int	*ft_convert_input(int ac, char **av)
 	int	*list;
 
 	i = -1;
-	list = ft_calloc(sizeof(int), ac);	
+	list = ft_calloc(sizeof(int), ac - 1);	
 	while (++i < (ac - 1))
 	{
 		list[i]	= ft_atoi(av[i + 1]);
 	}
 	return(list);
+}
+
+void	ft_ra(t_stks *lists, int ac)
+{
+	int	temp;
+	int	i;
+	
+	i = -1;
+	temp = lists->a[0];
+	while (++i < ac - 2)
+		lists->a[i] = lists->a[i + 1];
+	lists->a[i] = temp;
+	ft_putendl_fd("ra", 1);
+}
+
+void	ft_rb(t_stks *lists, int ac)
+{
+	int	temp;
+	int	i;
+	
+	i = -1;
+	temp = lists->b[0];
+	while (++i < ac - 2)
+		lists->b[i] = lists->b[i + 1];
+	lists->b[i] = temp;
+	ft_putendl_fd("rb", 1);
+}
+
+void	ft_rr(t_stks *lists, int ac)
+{
+	int	temp;
+	int	i;
+	
+	i = -1;
+	temp = lists->a[0];
+	while (++i < ac - 2)
+		lists->a[i] = lists->a[i + 1];
+	lists->a[i] = temp;
+	i = -1;
+	temp = lists->b[0];
+	while (++i < ac - 2)
+		lists->b[i] = lists->b[i + 1];
+	lists->b[i] = temp;
+	ft_putendl_fd("rr", 1);
+}
+void	ft_sa(t_stks *lists)
+{
+	int temp;
+
+	temp = lists->a[1];
+	lists->a[1] = lists->a[0];
+	lists->a[0] = temp;
+	ft_putendl_fd("sa", 1);
+}
+
+void	ft_sb(t_stks *lists)
+{
+	int temp;
+
+	temp = lists->b[1];
+	lists->b[1] = lists->b[0];
+	lists->b[0] = temp;
+	ft_putendl_fd("sb", 1);
+}
+
+void	ft_ss(t_stks *lists)
+{
+	int temp;
+
+	temp = lists->a[1];
+	lists->a[1] = lists->a[0];
+	lists->a[0] = temp;
+	temp = lists->b[1];
+	lists->b[1] = lists->b[0];
+	lists->b[0] = temp;
+	ft_putendl_fd("ss", 1);
 }
 
 void	ft_check_input(int ac, char **av)
@@ -61,23 +137,30 @@ void	ft_check_input(int ac, char **av)
 
 int	main(int ac, char **av)
 {
-	int	*list_a;
-	int	*list_b;
-	int	i;
+	t_stks	lists;
+	int		i;
 
 	if (ac == 1)
 		exit(0);
 	i = -1;
 	ft_check_input(ac, av);
-	list_b = ft_calloc(sizeof(int), ac);	
-	list_a = ft_convert_input(ac, av);
-	while (list_a[++i])
+	lists.b = ft_calloc(sizeof(int), ac);	
+	lists.a = ft_convert_input(ac, av);
+	while (++i < ac - 1)
 	{
-		ft_putnbr_fd(list_a[i], 1);
+		ft_putnbr_fd(lists.a[i], 1);
 		ft_putchar_fd(' ', 1);
 	}
 	ft_putchar_fd('\n', 1);
-	ft_putnbr_fd(list_b[0], 1);
+	i = -1;
+	ft_ra(&lists, ac);
+	while (++i < ac - 1)
+	{
+		ft_putnbr_fd(lists.a[i], 1);
+		ft_putchar_fd(' ', 1);
+	}
+	ft_putchar_fd('\n', 1);
+	ft_putnbr_fd(lists.b[0], 1);
 	ft_putchar_fd('\n', 1);
 	return (0);
 }
