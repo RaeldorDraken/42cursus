58a115e8d01b654217e14bc047c61ac7174bc1ff
Raeldor <maxshadow@hotmail.com>
2022-08-11 13:25:42 +0200

    I decided to remove the linked lists I couldnt think of a way to make them work without loosing track of the current position

--- push_swap/src/main.c
+++ push_swap/src/main.c
@@ -18,15 +18,18 @@ void	error_terminate(void)
 	exit(1);
 }
 
-t_list	*ft_convert_input(int ac, char **av, t_list *list)
+int	*ft_convert_input(int ac, char **av)
 {
 	int	i;
+	int	*list;
 
-	i = 1;
-	list = ft_lstnew(ft_atoi(av[i]));
-	while (++i < ac)
-		ft_lstadd_back(&list, ft_lstnew(ft_atoi(av[i])));
-	return (list);
+	i = -1;
+	list = ft_calloc(sizeof(int), ac);	
+	while (++i < (ac - 1))
+	{
+		list[i]	= ft_atoi(av[i + 1]);
+	}
+	return(list);
 }
 
 void	ft_check_input(int ac, char **av)
@@ -58,12 +61,23 @@ void	ft_check_input(int ac, char **av)
 
 int	main(int ac, char **av)
 {
-	t_list	list;
+	int	*list_a;
+	int	*list_b;
+	int	i;
 
 	if (ac == 1)
 		exit(0);
+	i = -1;
 	ft_check_input(ac, av);
-	list = *ft_convert_input(ac, av, &list);
-	ft_lstiter_put(&list);
+	list_b = ft_calloc(sizeof(int), ac);	
+	list_a = ft_convert_input(ac, av);
+	while (list_a[++i])
+	{
+		ft_putnbr_fd(list_a[i], 1);
+		ft_putchar_fd(' ', 1);
+	}
+	ft_putchar_fd('\n', 1);
+	ft_putnbr_fd(list_b[0], 1);
+	ft_putchar_fd('\n', 1);
 	return (0);
 }
