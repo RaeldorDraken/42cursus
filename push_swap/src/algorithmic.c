86561bb2ee02016599ac05675268241058d42ac3
Eduard Ros giralt <eros-gir@car3s1.42barcelona.com>
2022-10-13 11:22:49 +0200

    Solved the sort b thing! i was overcomplicating stuff, also did manage to reverse the order on the list so when its time to PA it will push biggest to smallest

--- push_swap/src/algorithmic.c
+++ push_swap/src/algorithmic.c
@@ -6,7 +6,7 @@
 /*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 11:22:05 by eros-gir          #+#    #+#             */
-/*   Updated: 2022/10/12 14:37:11 by eros-gir         ###   ########.fr       */
+/*   Updated: 2022/10/13 11:19:35 by eros-gir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,37 +36,29 @@ int	ft_rra_nbr_moves(t_stks *lists)
 	int	i;
 	int	j;
 
-	i = lists->size_a - 1;
-	while (i >= 0)
+	i = 1;
+	while (i < lists->size_a)
 	{
 		j = -1;
 		while (++j < 20)
 		{
-			if (lists->a[i] == lists->chunk[j])
-				return (-1 * (i - 1 - lists->size_a));
+			if (lists->a[lists->size_a - i] == lists->chunk[j])
+				return (i);
 		}
-		i --;
+		i ++;
 	}
 	return (lists->size_a);
 }
-//do a sort b after pb, with checking if its the biggest numberand making sa
-//and rb or rrb to move round the number to the right position.
-
-//or i can check for the biggest or smallest of the chunk and push that number
-//so can be pushed on top of the stack correctly that will be easier
 
 int	ft_sort_b(t_stks *lists, long int target)
 {
 	int	i;
-	int	buffer;
 
 	i = -1;
-	buffer = 0;
 	if (lists->size_b < 2)
 		return (i);
 	if (target == 9999999999)
 	{
-		buffer = 1;
 		ft_putchar_fd('\n', 1);
 		while (++i < lists->size_b)
 		{
@@ -82,9 +74,7 @@ int	ft_sort_b(t_stks *lists, long int target)
 		if (lists->b[i] == target)
 			break ;
 	}
-	if (buffer == 0)
-		i++;
-	return (i - buffer);
+	return (i);
 }
 
 void	test_funct(t_stks *lists, char type)
@@ -139,84 +129,37 @@ void    ft_pa_bottom(t_stks *lists)
 {
     int 		i;
 	long int	temp;
-	char		mode;
 
 	temp = ft_check_nearest(lists, lists->hold_bot);
-	ft_putnbr_fd(temp, 1);
-	ft_putchar_fd('\t', 1);
-	ft_putnbr_fd(lists->hold_top, 1);
 	i = ft_sort_b(lists, temp);
-	ft_putchar_fd('\t', 1);
-	ft_putnbr_fd(i, 1);
-	ft_putchar_fd('\n', 1);
-	if (i > lists->size_b / 2)
-	{
-		i = lists->size_b - i - 1;
-		mode = 'b';
-	}
-	else
-		mode = 't';
-	test_funct(lists, 'b');
 	while (lists->a[0] != lists->hold_bot)
-	{
-		if (i -- >= 0 && mode == 'b')
-		{
-			ft_rrr(lists);
-		}
-		else
-			ft_rra(lists);
-	}
-	while (i -- >= 0)
-	{
-		if (mode == 'b')
-			ft_rrb(lists);
-		else
-			ft_rb(lists);
-	}
+		ft_rra(lists);
+	while (i -- > 0)
+		ft_rb(lists);
 	ft_pb(lists);
-	test_funct(lists, 'b');
 }
 
 void	ft_pa_top(t_stks *lists)
 {
 	int			i;
 	long int	temp;
-	char		mode;
 
 	temp = ft_check_nearest(lists, lists->hold_top);
-	ft_putnbr_fd(temp, 1);
-	ft_putchar_fd('\t', 1);
-	ft_putnbr_fd(lists->hold_top, 1);
 	i = ft_sort_b(lists, temp);
-	ft_putchar_fd('\t', 1);
-	ft_putnbr_fd(i, 1);
-	ft_putchar_fd('\n', 1);
-	if (i > lists->size_b / 2)
-	{
-		i = lists->size_b - i - 1;
-		mode = 'b';
-	}
-	else
-		mode = 't';
-	test_funct(lists, 't');
 	while (lists->a[0] != lists->hold_top)
 	{
-		if (i -- >= 0 && mode == 't')
+		if (i -- > 0)
 		{
 			ft_rr(lists);
 		}
 		else
 			ft_ra(lists);
 	}
-	while (i -- >= 0)
+	while (i -- > 0)
 	{
-		if (mode == 't')
-			ft_rb(lists);
-		else
-			ft_rrb(lists);
+		ft_rb(lists);
 	}
 	ft_pb(lists);
-	test_funct(lists, 't');
 }
 
 void	ft_case_algorithmic(t_stks *lists)
