f95b0fdd21315f0fd2fca6ab590f92734c021e5b
Eduard Ros giralt <eros-gir@car3s1.42barcelona.com>
2022-11-14 11:19:43 +0100

    uploading the start of philosophers mandatory

---
+++ philosophers/philo/philolib.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philolib.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 10:59:12 by eros-gir          #+#    #+#             */
+/*   Updated: 2022/11/14 11:07:15 by eros-gir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOLIB_H
+# define PHILOLIB_H
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/time.h>
+#include<pthread.h>
+
+#endif
