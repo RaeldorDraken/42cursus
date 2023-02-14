b870f8934344d84ebebba87850d2d7a1af32285b
Eduard Ros giralt <eros-gir@car14s4.42barcelona.com>
2023-02-14 11:14:43 +0100

    new test for github key

--- minishell/src/ms_main.c
+++ minishell/src/ms_main.c
@@ -6,7 +6,7 @@
 /*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 10:05:31 by eros-gir          #+#    #+#             */
-/*   Updated: 2023/01/30 10:22:29 by eros-gir         ###   ########.fr       */
+/*   Updated: 2023/02/14 11:14:09 by eros-gir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 int	main(void)
 {
-	ft_putstr_fd("Hello world", 1);
+	ft_putendl_fd("Hello world", 1);
 }
