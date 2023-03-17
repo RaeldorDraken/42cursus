7d79ba0a7640295fd69c67efdadc810b67fb022d
Eduard Ros <maxshadow@hotmail.com>
2023-03-18 00:15:41 +0100

    Touched the split function with a temp var but changed it still work on the in_quote stuff

--- minishell/src/ms_lexer.c
+++ minishell/src/ms_lexer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ms_lexer.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eros-gir <eros-gir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: raeldor <raeldor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 11:18:17 by eros-gir          #+#    #+#             */
-/*   Updated: 2023/03/04 12:07:16 by eros-gir         ###   ########.fr       */
+/*   Updated: 2023/03/18 00:14:43 by raeldor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,6 +94,41 @@ char	*msh_getquotes(const char *s, char c, int qt)
 	return (msh_strset(s, c));
 }
 
+//Test over split Try it if the in_quotes works
+/*char **msh_split(char c, t_vars *vars, int quotes)
+{
+	char **strings;
+	size_t n;
+	size_t strn;
+	int i;
+	int in_quote = 0;
+
+	n = 0;
+	i = -1;
+	strn = msh_strnum(vars->inputline, c);
+	strings = ft_calloc(sizeof(char **), strn + 1);
+	if (!strings)
+		return (0);
+	while (n < strn)
+	{
+		quotes = msh_check_quotes(vars->inputline[++i]);
+		if (quotes != 3)
+		{
+			while (vars->inputline[++i] == c || vars->inputline[i] == '\'' || vars->inputline[i] == '\"')
+			{
+				quotes = msh_check_quotes(vars->inputline[i]);
+				if (in_quote && quotes == in_quote)
+					in_quote = 0; // found end quote
+				else if (!in_quote && quotes)
+					in_quote = quotes; // found start quote
+			}
+		}
+		strings[n] = msh_getquotes(vars->inputline, c, quotes);
+		vars->inputline += ft_strlen(strings[n++]);
+	}
+	return (strings);
+}*/
+
 char	**msh_split(char c, t_vars *vars, int quotes)
 {
 	char	**strings;
@@ -113,7 +148,7 @@ char	**msh_split(char c, t_vars *vars, int quotes)
 		if (quotes != 3)
 		{
 			while (vars->inputline[++i] == c || vars->inputline[i] == '\''
-					|| vars->inputline[i] == '\"')
+				|| vars->inputline[i] == '\"')
 				quotes = msh_check_quotes(vars->inputline[i]);
 		}
 		strings[n] = msh_getquotes(vars->inputline, c, quotes);
