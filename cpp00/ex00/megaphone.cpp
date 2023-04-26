256cc7d4f777cc285c41f0bea797fd27a81d348c
Eduard Ros giralt <eros-gir@car12s2.42barcelona.com>
2023-04-26 10:27:15 +0200

    ex00 of cpp module done

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,11 +6,28 @@
 /*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/23 21:34:37 by eros-gir          #+#    #+#             */
-/*   Updated: 2023/04/23 21:39:43 by eros-gir         ###   ########.fr       */
+/*   Updated: 2023/04/26 10:24:26 by eros-gir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<iostream>
 
-int	main(void) {
+int	main(int ac, char** av) 
+{
+	if (ac <= 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	else
+	{
+		for (int i = 1; av[i]; i++)
+		{
+			for (int j = 0; av[i][j]; j++)
+			{
+				std::cout << (char)std::toupper(av[i][j]);
+			}
+		}
+		std::cout << std::endl;
+	}
+	return (0);
 }
