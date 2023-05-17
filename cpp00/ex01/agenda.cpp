d411e42255aa1e8d0077eb1e00b8c19bf1253d0e
Eduard Ros giralt <eros-gir@cbr9s2.42barcelona.com>
2023-05-17 11:52:45 +0200

    cpp00 ex01 fixing input

--- cpp00/ex01/agenda.cpp
+++ cpp00/ex01/agenda.cpp
@@ -6,7 +6,7 @@
 /*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 10:30:57 by eros-gir          #+#    #+#             */
-/*   Updated: 2023/05/17 10:13:52 by eros-gir         ###   ########.fr       */
+/*   Updated: 2023/05/17 11:50:32 by eros-gir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,11 @@ int	main(void)
 		std::cout << "Enter a command: ";
 		std::cin >> buff;
 		std::cout << "You entered: [" << buff << "]" << std::endl;
-		if (!std::strcmp(buff, "ADD") || !std::strcmp(buff, "add"))
+		if (!std::strcmp(buff, "ADD") || !std::strcmp(buff, "add") || !std::strcmp(buff, "Add"))
 			phonebook.ADD(&phonebook);
-		else if (!std::strcmp(buff, "EXIT") || !std::strcmp(buff, "exit"))
+		else if (!std::strcmp(buff, "EXIT") || !std::strcmp(buff, "exit") || !std::strcmp(buff, "Exit"))
 			phonebook.EXIT(&phonebook);
-		else if (!std::strcmp(buff, "SEARCH") || !std::strcmp(buff, "search"))
+		else if (!std::strcmp(buff, "SEARCH") || !std::strcmp(buff, "search") || !std::strcmp(buff, "Search"))
 			phonebook.SEARCH(&phonebook);
 		else	std::cout << "Invalid command" << std::endl;
 	}
