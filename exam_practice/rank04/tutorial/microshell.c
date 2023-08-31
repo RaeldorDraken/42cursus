b6ac510fabb68ee6e8704f44ab0c2224a83371e3
Eduard Ros giralt <eros-gir@car14s1.42barcelona.com>
2023-08-31 18:23:22 +0200

    updating microshell for testing purposes

--- exam_practice/rank04/tutorial/microshell.c
+++ exam_practice/rank04/tutorial/microshell.c
@@ -6,79 +6,103 @@
 /*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 19:00:49 by eros-gir          #+#    #+#             */
-/*   Updated: 2023/08/29 19:19:02 by eros-gir         ###   ########.fr       */
+/*   Updated: 2023/08/31 18:22:51 by eros-gir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
 
-int	err(char *str)
+/*not needed in exam, but necessary if you want to use this tester:
+https://github.com/Glagan/42-exam-rank-04/blob/master/microshell/test.sh*/
+// #ifdef TEST_SH
+// # define TEST		1
+// #else
+// # define TEST		0
+// #endif
+
+int	ft_putstr_fd2(char *str, char *arg)
 {
 	while (*str)
 		write(2, str++, 1);
+	if (arg)
+		while(*arg)
+			write(2, arg++, 1);
+	write(2, "\n", 1);
 	return (1);
 }
 
-int	cd(char **argv, int i)
+int ft_execute(char *argv[], int i, int tmp_fd, char *env[])
 {
-	if (i != 2)
-		return (err("error: cd: bad arguments\n"));
-	else if (chdir(argv[1]) == -1)
-		return (err("error: cd: cannot change directory to "), 
-			err(argv[1]), err("\n"));
-	return (0);
+	//overwrite ; or | or NULL with NULL to use the array as input for execve.
+	//we are here in the child so it has no impact in the parent process.
+	argv[i] = NULL;
+	dup2(tmp_fd, STDIN_FILENO);
+	close(tmp_fd);
+	execve(argv[0], argv, env);
+	return (ft_putstr_fd2("error: cannot execute ", argv[0]));
 }
 
-int	exec(char **argv, char **envp, int i)
-{
-	int	fd[2];
-	int	status;
-	int	has_pipe;
-	int	pid;
-
-	has_pipe = argv[i] && !strcmp(argv[i], "|");
-	if (has_pipe && pipe(fd) == -1)
-		return (err("error: fatal\n"));
-
-	pid = fork();
-	if (!pid)
-	{
-		argv[i] = 0;
-		if (has_pipe && (dup2(fd[1], 1) == -1
-				|| close(fd[0]) == -1 || close(fd[1]) == -1))
-			return (err("error: fatal\n"));
-		execve(*argv, argv, envp);
-		return (err("error: cannot execute "), err(*argv), err("\n"));
-	}
-	waitpid(pid, &status, 0);
-	if (has_pipe && (dup2(fd[0], 0) == -1 
-			|| close(fd[0]) == -1 || close(fd[1]) == -1))
-		return (err("error: fatal\n"));
-	return (WIFEXITED(status) && WEXITSTATUS(status));
-}
-
-int	main(int argc, char **argv, char **envp)
+int	main(int argc, char *argv[], char *env[])
 {
 	int	i;
-	int	status;
+	int fd[2];
+	int tmp_fd;
+	(void)argc;	// is needed in exam, because the exam tester compiles with -Wall -Wextra -Werror
 
 	i = 0;
-	status = 0;
-	if (argc > 1) 
+	tmp_fd = dup(STDIN_FILENO);
+	while (argv[i] && argv[i + 1]) //check if the end is reached
 	{
-		while (argv[i] && argv[++i]) 
+		argv = &argv[i + 1];	//the new argv start after the ; or |
+		i = 0;
+		//count until we have all informations to execute the next child;
+		while (argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
+			i++;
+		if (strcmp(argv[0], "cd") == 0) //cd
 		{
-			argv += i;
-			i = 0;
-			while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
-				i++;
-			if (!strcmp(*argv, "cd"))
-				status = cd(argv, i);
-			else if (i)
-				status = exec(argv, envp, i);
+			if (i != 2)
+				ft_putstr_fd2("error: cd: bad arguments", NULL);
+			else if (chdir(argv[1]) != 0)
+				ft_putstr_fd2("error: cd: cannot change directory to ", argv[1]	);
+		}
+		else if (i != 0 && (argv[i] == NULL || strcmp(argv[i], ";") == 0)) //exec in stdout
+		{
+			if ( fork() == 0)
+			{
+				if (ft_execute(argv, i, tmp_fd, env))
+					return (1);
+			}
+			else
+			{
+				close(tmp_fd);
+				while(waitpid(-1, NULL, WUNTRACED) != -1)
+					;
+				tmp_fd = dup(STDIN_FILENO);
+			}
+		}
+		else if(i != 0 && strcmp(argv[i], "|") == 0) //pipe
+		{
+			pipe(fd);
+			if ( fork() == 0)
+			{
+				dup2(fd[1], STDOUT_FILENO);
+				close(fd[0]);
+				close(fd[1]);
+				if (ft_execute(argv, i, tmp_fd, env))
+					return (1);
+			}
+			else
+			{
+				close(fd[1]);
+				close(tmp_fd);
+				tmp_fd = fd[0];
+			}
 		}
 	}
-	return (status);
+	close(tmp_fd);
+	// if (TEST)		// not needed in exam, but necessary if you want to use this tester:
+	// 	while (1);	// https://github.com/Glagan/42-exam-rank-04/blob/master/microshell/test.sh
+	return (0);
 }
