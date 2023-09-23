efe08eff60ce7e0b558a1f49d80a60b819206874
Eduard Ros giralt <eros-gir@cbr9s1.42barcelona.com>
2023-09-23 18:34:40 +0200

    my microshell works with grademe now

--- exam_practice/rank04/microshell.c
+++ exam_practice/rank04/microshell.c
@@ -6,7 +6,7 @@
 /*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 16:50:45 by eros-gir          #+#    #+#             */
-/*   Updated: 2023/09/23 15:26:32 by eros-gir         ###   ########.fr       */
+/*   Updated: 2023/09/23 16:03:14 by eros-gir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,80 +19,77 @@ int	print_err(char *str, char *arg)
 	while (*str)
 		write(2, str++, 1);
 	if (arg)
-	{
 		while (*arg)
 			write(2, arg++, 1);
-	}
 	write(2, "\n", 1);
 	return (1);
 }
 
-int	execution(char **argv, int i, int tmp_fd, char **envp)
+int	execution(char *av[], int i, int old_fd, char *env[])
 {
-	argv[i] = NULL;
-	dup2(tmp_fd, STDIN_FILENO);
-	close(tmp_fd);
-	execve(argv[0], argv, envp);
-	return (print_err("error: cannot execute ", argv[0]));
+	av[i] = NULL;
+	dup2(old_fd, 0);
+	close(old_fd);
+	execve(av[0], av, env);
+	return (print_err("error: cannot execute ", av[0]));
 }
 
-int	main(int argc, char **argv, char **envp)
+int	main(int ac, char *av[], char *env[])
 {
 	int	i;
 	int	pipe_fd[2];
 	int	old_fd;
-	(void)argc;
 
-	(void)argc;
+	(void)ac;
 	i = 0;
-	old_fd = dup(STDIN_FILENO);
-	while (argv[i] && argv[i + 1])
+	old_fd = dup(0);
+	while (av[i] && av[i + 1])
 	{
-		argv = &argv[i + 1];
+		av = &av[i + 1];
 		i = 0;
-		while (argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
+		while (av[i] && strcmp(av[i], ";") && strcmp(av[i], "|"))
 			i++;
-		if (strcmp(argv[0], "cd") == 0)
+		if (strcmp(av[0], "cd") == 0)
 		{
 			if (i != 2)
-				return (print_err("error: cd: bad arguments", NULL));
-			else if (chdir(argv[1]) != 0)
-				return (print_err("error: cd: cannot change directory to ", argv[1]));
+				print_err("error: cd: bad arguments", NULL);
+			else if (chdir(av[i]) != 0)
+				print_err("error: cd: cannot change directory to ", av[i]);
 		}
-		else if (i != 0 && (argv[i] == NULL || strcmp(argv[i], ";") == 0))
+		else if (i != 0 && (av[i] == NULL || strcmp(av[i], ";") == 0))
 		{
 			if (fork() == 0)
 			{
-				if (execution(argv, i, tmp_fd, envp))
+				if (execution(av, i, old_fd, env))
 					return (1);
 			}
 			else
 			{
-				close(tmp_fd);
+				close(old_fd);
 				while (waitpid(-1, NULL, WUNTRACED) != -1)
 					;
-				tmp_fd = dup(STDIN_FILENO);
+				old_fd = dup(0);
 			}
 		}
-		else if (i != 0 && strcmp(argv[i], "|") == 0)
+		else if (i != 0 && strcmp(av[i], "|") == 0)
 		{
 			pipe(pipe_fd);
 			if (fork() == 0)
 			{
-				dup2(pipe_fd[1], STDOUT_FILENO);
+				dup2(pipe_fd[1], 1);
 				close(pipe_fd[0]);
 				close(pipe_fd[1]);
-				if (execution(argv, i, tmp_fd, envp))
+				if (execution(av, i, old_fd, env))
 					return (1);
 			}
 			else
 			{
 				close(pipe_fd[1]);
-				close(tmp_fd);
-				tmp_fd = pipe_fd[0];
+				close(old_fd);
+				old_fd = pipe_fd[0];
 			}
 		}
 	}
-	close(tmp_fd);
+	close(old_fd);
 	return (0);
 }
