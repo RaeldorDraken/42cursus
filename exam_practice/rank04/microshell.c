374f7581f6b788ae228f036edc250cb13fe8cf4a
Eduard Ros giralt <eros-gir@cbr9s1.42barcelona.com>
2023-09-23 15:24:52 +0200

    shitty update

--- exam_practice/rank04/microshell.c
+++ exam_practice/rank04/microshell.c
@@ -6,7 +6,7 @@
 /*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 16:50:45 by eros-gir          #+#    #+#             */
-/*   Updated: 2023/09/08 20:56:55 by eros-gir         ###   ########.fr       */
+/*   Updated: 2023/09/17 19:47:17 by eros-gir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,11 @@ int	print_err(char *str, char *arg)
 	return (1);
 }
 
-int	execution(char **argv, int i, int old_fd, char **envp)
+int	execution(char **argv, int i, int tmp_fd, char **envp)
 {
 	argv[i] = NULL;
-	dup2(old_fd, STDIN_FILENO);
-	close(old_fd);
+	dup2(tmp_fd, STDIN_FILENO);
+	close(tmp_fd);
 	execve(argv[0], argv, envp);
 	return (print_err("error: cannot execute ", argv[0]));
 }
@@ -40,12 +40,11 @@ int	main(int argc, char **argv, char **envp)
 {
 	int	i;
 	int	pipe_fd[2];
-	int	old_fd;
+	int	tmp_fd;
 
+	(void)argc;
 	i = 0;
-	old_fd = dup(STDIN_FILENO);
-	if (argc <= 1)
-		return (0);
+	tmp_fd = dup(STDIN_FILENO);
 	while (argv[i] && argv[i + 1])
 	{
 		argv = &argv[i + 1];
@@ -63,15 +62,15 @@ int	main(int argc, char **argv, char **envp)
 		{
 			if (fork() == 0)
 			{
-				if (execution(argv, i, old_fd, envp))
+				if (execution(argv, i, tmp_fd, envp))
 					return (1);
 			}
 			else
 			{
-				close(old_fd);
+				close(tmp_fd);
 				while (waitpid(-1, NULL, WUNTRACED) != -1)
 					;
-				old_fd = dup(STDIN_FILENO);
+				tmp_fd = dup(STDIN_FILENO);
 			}
 		}
 		else if (i != 0 && strcmp(argv[i], "|") == 0)
@@ -82,17 +81,17 @@ int	main(int argc, char **argv, char **envp)
 				dup2(pipe_fd[1], STDOUT_FILENO);
 				close(pipe_fd[0]);
 				close(pipe_fd[1]);
-				if (execution(argv, i, old_fd, envp))
+				if (execution(argv, i, tmp_fd, envp))
 					return (1);
 			}
 			else
 			{
 				close(pipe_fd[1]);
-				close(old_fd);
-				old_fd = pipe_fd[0];
+				close(tmp_fd);
+				tmp_fd = pipe_fd[0];
 			}
 		}
 	}
-	close(old_fd);
+	close(tmp_fd);
 	return (0);
 }
