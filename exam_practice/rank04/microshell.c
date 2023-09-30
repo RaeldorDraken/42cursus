fd86a6030df82c9406cc16f22d0c3c2f8e48e937
Eduard Ros giralt <eros-gir@cbr15s1.42barcelona.com>
2023-09-30 20:14:56 +0200

    fixed a problem with chdir input

--- exam_practice/rank04/microshell.c
+++ exam_practice/rank04/microshell.c
@@ -6,7 +6,7 @@
 /*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 16:50:45 by eros-gir          #+#    #+#             */
-/*   Updated: 2023/09/23 16:03:14 by eros-gir         ###   ########.fr       */
+/*   Updated: 2023/09/30 20:13:19 by eros-gir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ int	main(int ac, char *av[], char *env[])
 			if (i != 2)
 				print_err("error: cd: bad arguments", NULL);
 			else if (chdir(av[i]) != 0)
-				print_err("error: cd: cannot change directory to ", av[i]);
+				print_err("error: cd: cannot change directory to ", av[1]);
 		}
 		else if (i != 0 && (av[i] == NULL || strcmp(av[i], ";") == 0))
 		{
