a730ad539d0d810567d9880e5d236e71a3830069
Eduard Ros <maxshadow@hotmail.com>
2023-10-09 11:16:14 +0200

    sed for losers terminado, sse ha de probar en el campus ya que he cambiado cosas para que funcione en windows

--- cpp01/ex04/SedForLosers.hpp
+++ cpp01/ex04/SedForLosers.hpp
@@ -6,7 +6,7 @@
 /*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 18:00:16 by eros-gir          #+#    #+#             */
-/*   Updated: 2023/10/08 19:14:46 by eros-gir         ###   ########.fr       */
+/*   Updated: 2023/10/09 11:13:18 by eros-gir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,13 @@ class StringReplace
 
 	private:
 		std::string	_filename;
+		std::string	_outfilename;
 		std::string	_s1;
 		std::string	_s2;
 		std::string _tempfile;
 		std::string line;
-		void set_internal_vars(std::string filename, std::string s1, std::string s2);
+		void	set_internal_vars(std::string filename, std::string s1, std::string s2);
+		void	replaceString();
 };
 
 #endif
