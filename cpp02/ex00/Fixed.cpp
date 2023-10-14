f7b35199ee24479c99f09a1f4e4b2e27f0122e76
Eduard Ros <maxshadow@hotmail.com>
2023-10-14 14:51:09 +0200

    starting fixed function on ex00

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,7 +6,43 @@
 /*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 20:11:22 by eros-gir          #+#    #+#             */
-/*   Updated: 2023/10/13 20:11:25 by eros-gir         ###   ########.fr       */
+/*   Updated: 2023/10/14 14:47:58 by eros-gir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->_fixed_point_value = 0;
+}
+
+Fixed::Fixed(const Fixed &src)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
+
+Fixed::Fixed(const int value)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->_fixed_point_value = value << this->_fractional_bits;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int		Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_fixed_point_value);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_fixed_point_value = raw;
+}
