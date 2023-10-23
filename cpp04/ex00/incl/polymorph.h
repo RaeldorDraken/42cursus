c1661f8c6b6c7de9eae9a99cc176a87bf0c52bb9
Eduard Ros <maxshadow@hotmail.com>
2023-10-23 11:31:43 +0200

    finished ex00 i think

--- cpp04/ex00/incl/polymorph.h
+++ cpp04/ex00/incl/polymorph.h
@@ -1,30 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   WrongCat.hpp                                       :+:      :+:    :+:   */
+/*   polymorph.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/22 15:57:58 by eros-gir          #+#    #+#             */
-/*   Updated: 2023/10/23 11:16:19 by eros-gir         ###   ########.fr       */
+/*   Created: 2023/10/23 11:04:56 by eros-gir          #+#    #+#             */
+/*   Updated: 2023/10/23 11:18:19 by eros-gir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WRONGCAT_HPP
-# define WRONGCAT_HPP
+#ifndef POLYMORPH_H
+# define POLYMORPH_H
 
+# include "Animal.hpp"
+# include "Dog.hpp"
+# include "Cat.hpp"
 # include "WrongAnimal.hpp"
-
-class WrongCat : public WrongAnimal
-{
-	public:
-		WrongCat();
-		WrongCat(WrongCat const &src);
-		~WrongCat();
-
-		WrongCat &operator=(WrongCat const &rhs);
-
-		void	makeSound(void) const;
-};
+# include "WrongCat.hpp"
 
 #endif
