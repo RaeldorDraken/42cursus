22a9ec0d47869ec093d6a42b10eaf67c7dc722f6
Eduard Ros <maxshadow@hotmail.com>
2023-11-24 17:41:54 +0100

    started doing the cpp06 its weird

---
+++ cpp06/ex00/ScalarConverter.cpp
@@ -0,0 +1,170 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/24 16:40:48 by eros-gir          #+#    #+#             */
+/*   Updated: 2023/11/24 17:41:12 by eros-gir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter(void)
+{
+	return ;
+}
+
+ScalarConverter::ScalarConverter(ScalarConverter const &src)
+{
+	*this = src;
+	return ;
+}
+
+ScalarConverter::~ScalarConverter(void)
+{
+	return ;
+}
+
+ScalarConverter	&ScalarConverter::operator=(ScalarConverter const &rhs)
+{
+	(void)rhs;
+	return (*this);
+}
+
+bool	ScalarConverter::isChar(std::string input)
+{
+	if (input.length() == 1 && !isdigit(input[0]))
+		return (true);
+	return (false);
+}
+
+bool	ScalarConverter::isInt(std::string input)
+{
+	if (input.length() == 1 && isdigit(input[0]))
+		return (true);
+	if (input.length() > 1 && (isdigit(input[0]) || input[0] == '-'))
+	{
+		for (size_t i = 1; i < input.length(); i++)
+		{
+			if (!isdigit(input[i]))
+				return (false);
+		}
+		return (true);
+	}
+	return (false);
+}
+
+bool	ScalarConverter::isFloat(std::string input)
+{
+	if (input.length() == 1 && isdigit(input[0]))
+		return (true);
+	if (input.length() > 1 && (isdigit(input[0]) || input[0] == '-'))
+	{
+		for (size_t i = 1; i < input.length(); i++)
+		{
+			if (!isdigit(input[i]) && input[i] != '.')
+				return (false);
+		}
+		return (true);
+	}
+	return (false);
+}
+
+bool	ScalarConverter::isDouble(std::string input)
+{
+	if (input.length() == 1 && isdigit(input[0]))
+		return (true);
+	if (input.length() > 1 && (isdigit(input[0]) || input[0] == '-'))
+	{
+		for (size_t i = 1; i < input.length(); i++)
+		{
+			if (!isdigit(input[i]) && input[i] != '.')
+				return (false);
+		}
+		return (true);
+	}
+	return (false);
+}
+
+bool	ScalarConverter::isInf(std::string input)
+{
+	if (input == "inf" || input == "+inf" || input == "-inf" || input == "nan")
+		return (true);
+	return (false);
+}
+
+void	ScalarConverter::printChar(char c)
+{
+	if (isprint(c))
+		std::cout << "char: '" << c << "'" << std::endl;
+	else
+		std::cout << "char: Non displayable" << std::endl;
+}
+
+void ScalarConverter::printInt(int n)
+{
+	if (n > std::numeric_limits<int>::max() 
+	|| n < std::numeric_limits<int>::min())
+		std::cout << "int: Out of range" << std::endl;
+	else
+    	std::cout << "int: " << n << std::endl;
+}
+
+void ScalarConverter::printFloat(float f)
+{
+    if (std::abs(f) > std::numeric_limits<float>::max())
+	        std::cout << "float: Out of range" << std::endl;
+    else
+	{
+        std::cout << "float: " << std::fixed 
+		<< std::setprecision(1) << f << "f" << std::endl;
+	}
+}
+
+void ScalarConverter::printDouble(double d)
+{
+    if (std::abs(d) > std::numeric_limits<double>::max())
+        std::cout << "double: Out of range" << std::endl;
+    else
+	{
+        std::cout << "double: " << std::fixed 
+		<< std::setprecision(1) << d << std::endl;
+	}
+}
+
+void	ScalarConverter::printInf(std::string input)
+{
+	if (input == "inf" || input == "+inf")
+	{
+		std::cout << "float: " << std::fixed 
+		<< std::setprecision(1) << std::numeric_limits<float>::infinity() 
+		<< "f" << std::endl;
+	}
+	else if (input == "-inf")
+	{
+		std::cout << "float: " << std::fixed 
+		<< std::setprecision(1) << -std::numeric_limits<float>::infinity() 
+		<< "f" << std::endl;
+	}
+	else if (input == "nan")
+		std::cout << "float: nanf" << std::endl;
+}
+
+void	ScalarConverter::convert(std::string input)
+{
+	if (isChar(input))
+		printChar(input[0]);
+	else if (isInt(input))
+		printInt(atoi(input.c_str()));
+	else if (isFloat(input))
+		printFloat(atof(input.c_str()));
+	else if (isDouble(input))
+		printDouble(strtod(input.c_str(), NULL));
+	else if (isInf(input))
+		printInf(input);
+	else
+		std::cout << "impossible" << std::endl;
+}
