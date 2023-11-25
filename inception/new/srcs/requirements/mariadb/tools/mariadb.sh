fc6d87d828d6cb0ee22affe6a8258eb02b467d41
Eduard Ros giralt <eros-gir@car13s6.42barcelona.com>
2023-11-25 18:34:42 +0100

    fixing everythinggit add .
    !

--- inception/new/srcs/requirements/mariadb/tools/mariadb.sh
+++ inception/new/srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,15 +1,14 @@
-#!/bin/bash
+#! /bin/bash
+service mariadb start
 
-service mysql start
-
-if [ ! -d /var/lib/mysql/${MYSQL_DATABASE} ]; then
-    mysql -u ${MYSQL_ROOT_USER} -p"${MYSQL_ROOT_PASSWORD}" -h localhost -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;"
-    mysql -u ${MYSQL_ROOT_USER} -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-    mysql -u ${MYSQL_ROOT_USER} -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
-    mysql -u ${MYSQL_ROOT_USER} -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
-    mysql -u ${MYSQL_ROOT_USER} -p"${MYSQL_ROOT_PASSWORD}" -e "ALTER USER '${MYSQL_ROOT_USER}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+if [ ! -d /var/lib/mysql/${MYSQL_DATABASE} ];
+then
+	mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE $MYSQL_DATABASE;"
+	mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
+	mysql -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
+	mysql -e "FLUSH PRIVILEGES;"
 fi
 
-mysqladmin -u ${MYSQL_ROOT_USER} --password="${MYSQL_ROOT_PASSWORD}" shutdown
+mysqladmin -u ${MYSQL_ROOT_USER} --password=${MYSQL_ROOT_PASSWORD} shutdown
 
 mysqld
