d3ef05482641d6f69cc08c8b90dc58ba8e77d8a0
Eduard Ros giralt <eros-gir@car4s1.42barcelona.com>
2023-12-06 19:44:43 +0100

    cpp07 is finished

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,13 +6,65 @@
 /*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 09:18:14 by eros-gir          #+#    #+#             */
-/*   Updated: 2023/12/06 18:40:42 by eros-gir         ###   ########.fr       */
+/*   Updated: 2023/12/06 19:44:17 by eros-gir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
 #include "Array.hpp"
 
+int main(void)
+{
+	Array<int>	a(5);
+
+	for (unsigned int i = 0; i < a.size(); i++)
+		a[i] = i;
+    std::cout << "Array a:" << std::endl;
+	for (unsigned int i = 0; i < a.size(); i++)
+		std::cout << a[i] << std::endl;
+	try
+	{
+		a[5] = 5;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Exception caught: " << e.what() << std::endl;
+	}
+
+    std::cout << "Array b created from a" << std::endl;
+    Array<int>	b;
+
+    b = a;
+    a[0] = 42;
+    std::cout << "Array a modified:" << std::endl;
+    for (unsigned int i = 0; i < a.size(); i++)
+        std::cout << a[i] << std::endl;
+
+    std::cout << "Array b:" << std::endl;
+    for (unsigned int i = 0; i < b.size(); i++)
+        std::cout << b[i] << std::endl;
+
+    // std::cout << "Array c constant" << std::endl;
+    // Array<int> c;
+    // c[] = {1, 2, 3, 4, 5};
+    // std::cout << "Array c:" << std::endl;
+    // for (unsigned int i = 0; i < c.size(); i++)
+    //     std::cout << c[i] << std::endl;
+
+    std::cout << "Array s" << std::endl;
+    Array<std::string> s(5);
+    s[0] = "Hello";
+    s[1] = "World";
+    s[2] = "!";
+    s[3] = "I'm";
+    s[4] = "a string";
+    std::cout << "Array s:" << std::endl;
+    for (unsigned int i = 0; i < s.size(); i++)
+        std::cout << s[i] << " ";
+    std::cout << std::endl;
+    
+	return (0);
+}
+/*
 #define MAX_VAL 750
 int main(int, char**)
 {
@@ -63,3 +115,4 @@ int main(int, char**)
     delete [] mirror;//
     return 0;
 }
+*/
