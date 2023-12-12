bd8ab34d78ceb0e332d3ed55d1350e9df5377773
Eduard Ros <maxshadow@hotmail.com>
2023-12-12 13:57:51 +0100

    ex00 of cpp08 finished? i need to test

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/12 10:47:41 by eros-gir          #+#    #+#             */
+/*   Updated: 2023/12/12 13:57:06 by eros-gir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+# include <algorithm>
+# include <exception>
+
+template <typename T>
+int	easyfind(T &array, int find);
+
+# include"easyfind.tpp"
+
+#endif
