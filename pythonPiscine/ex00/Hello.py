0bd142b65949f13fafa7ca4fe54db1023a84a762
Eduard Ros giralt <eros-gir@student.42barcelona.com>
2024-03-17 17:24:05 +0100

    python picine

---
+++ pythonPiscine/ex00/Hello.py
@@ -0,0 +1,26 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+#my code here
+
+index = 0
+while index < len(ft_list) - 1:
+	index += 1
+
+ft_list[index] = " world!"
+
+index = 0
+while index < len(ft_tuple) - 1:
+	index += 1
+
+ft_tuple = list(ft_tuple)
+ft_tuple[index] = " Spain!"
+
+#end of my code
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
